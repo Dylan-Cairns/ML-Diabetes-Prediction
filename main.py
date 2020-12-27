506c5661342200f9b8a968ff1a1b7defd2a280d4
diyanious <dcairns@wgu.edu>
2020-12-27 12:35:44 +0800

    fixed form formatting. Looking good!

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, Markup
 import pickle
+import numpy
 from form import DiagnoseForm
 
 app = Flask(__name__)
@@ -16,15 +17,17 @@ def home():
 def diagnose():
     form = DiagnoseForm()
     if form.validate_on_submit():
-        gender = (form.gender.data == 'True')
+        gender = (form.gender.data == 'True') # Convert boolean to string
         polyuria = form.polyuria.data
         polydipsia = form.polydipsia.data
         sudden_wl = form.sudden_wl.data
         obesity = form.obesity.data
         features = [gender, polyuria, polydipsia, sudden_wl, obesity]
-        prediction = model.predict([features])
-        return render_template("diagnose.html", form=form, data=prediction, title='Diagnose')
-    return render_template("diagnose.html", form=form, data='', title='Diagnose')
+        prediction = 'Positive' if model.predict([features]) else 'Negative' # Convert boolean to string
+        accuracy = round((numpy.max(model.predict_proba([features])) / 1), 2)
+        flash(Markup(render_template("result.html", prediction=prediction, accuracy=accuracy)))
+        return render_template("diagnose.html", form=form, title='Diagnose')
+    return render_template("diagnose.html", form=form, title='Diagnose')
 
 
 @app.route("/data")
