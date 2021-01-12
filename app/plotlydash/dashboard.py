3d380744c2a7fb38f919b992ac4ee2ddf8b9ed2d
diyanious <dcairns@wgu.edu>
2021-01-12 16:21:53 +0800

    finally got scatter plot working

--- app/plotlydash/dashboard.py
+++ app/plotlydash/dashboard.py
@@ -7,6 +7,7 @@ import dash_html_components as html
 import dash_core_components as dcc
 import plotly.graph_objects as go
 import plotly.figure_factory as ff
+import plotly.io as pio
 import plotly.express as px
 from .data import create_dataframe
 from .layout import html_layout
@@ -28,23 +29,26 @@ def init_dashboard(server):
     # Create confusion matrix
     feature_heatmap_fig = px.imshow(df.corr(), color_continuous_scale=px.colors.sequential.ice)
 
-    # 3d confusion matric wooaaah!
-    threedee_fig_ohyea = go.Figure(data=[go.Surface(z=df.corr())])
-
     # Create feature importance bar chart
     feature_importance = pd.Series(rf_model.feature_importances_*100,
-                                   index=df.columns[:-1])
-    feature_importance = feature_importance.sort_values(ascending=False)
+                                   index=df.columns[:-1]).sort_values(ascending=False)
     feat_import_fig = px.bar(feature_importance,
-                             labels={'value':'Importance', 'index': 'Features'})
+                             labels={'value': 'Importance', 'index': 'Features'})
     feat_import_fig.layout.update(showlegend=False)
 
     # Create age/feature scatter plot
-    #scatter_plot_fig = px.scatter(x=df['Age'], y=df['class'])
-
-    # Create feature count bar chart
-    #feature_df = df.drop('Age', axis=1)
-    # feature_count_fig = px.bar(feature_df, x=df.columns, color="class", barmode="group")
+    x_graph = df[df['class'] == True]['Age'].value_counts().index
+    x_graph2 = x_graph
+    y_graph = df[df['class'] == True]['Age'].value_counts().values
+    fig_compare = go.Figure()
+    fig_compare.add_trace(go.Scatter(x=x_graph, y=y_graph,
+                             mode='markers',
+                             name='Diabetes'))
+    x_graphb = df[df['Polyuria'] == True]['Age'].value_counts().index
+    y_graphb = df[df['Polyuria'] == True]['Age'].value_counts().values
+    fig_compare.add_trace(go.Scatter(x=x_graphb, y=y_graphb,
+                             mode='markers',
+                             name='Polyuria'))
 
     # Custom HTML layout
     dash_app.index_string = html_layout
@@ -52,9 +56,9 @@ def init_dashboard(server):
     # Create Layout
     dash_app.layout = html.Div(
         children=[
-            dcc.Graph(figure=threedee_fig_ohyea),
             dcc.Graph(figure=feature_heatmap_fig),
             dcc.Graph(figure=feat_import_fig),
+            dcc.Graph(figure=fig_compare),
             create_data_table(df),
         ],
         id='dash-container'
