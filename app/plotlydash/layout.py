e1aab7767f309de9c718d8eeee3c33cd3c394071
diyanious <dcairns@wgu.edu>
2021-01-13 21:34:49 +0800

    working on restyle

--- app/plotlydash/layout.py
+++ app/plotlydash/layout.py
@@ -20,7 +20,8 @@ html_layout = """
     <title>Data + Visualizations</title>
 </head>
 <body class="d-flex flex-column h-100 body">
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary bg-gradient shadow">
+<div style="background-color: #000207"><div style="background-image: url('../static/ai_background_trans.jpg'); background-size: 100% auto; background-position: right top; background-repeat: repeat;">
+<nav class="navbar navbar-expand-lg navbar-dark bg-transparent">
     <div class="container">
         <a class="navbar-brand" href="#"><span class="fa fa-medkit"></span> ML Diabetes Diagnosis</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -68,6 +69,7 @@ html_layout = """
                 {%scripts%}
                 {%renderer%}
 </footer>
+</div></div>
 <!-- bootstrap Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 
